c2e307b6e98fdd7f38caca19b2003a369e3fbef3
Porter Hatch <porterhatch1@gmail.com>
2023-09-27 14:28:53 -0600

    working on the word processor being able to do a few things

--- src/wordProcessor.jsx
+++ src/wordProcessor.jsx
@@ -1,16 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function WordProcessor() {
   const [content, setContent] = useState('');
+  const [undoStack, setUndoStack] = useState([]);
+  const [redoStack, setRedoStack] = useState([]);
+  const textAreaRef = useRef(null);
+
+  const applyFormatting = (formatting) => {
+    const textarea = textAreaRef.current;
+    const startPos = textarea.selectionStart;
+    const endPos = textarea.selectionEnd;
+    const text = textarea.value;
+
+    const formattedText = text.substring(0, startPos) + formatting + text.substring(startPos, endPos) + text.substring(endPos);
+
+    setContent(formattedText);
+    textarea.focus();
+    textarea.setSelectionRange(startPos, endPos + formatting.length);
+  };
 
   const handleContentChange = (e) => {
-    setContent(e.target.value);
+    const newContent = e.target.value;
+    setUndoStack([...undoStack, content]);
+    setRedoStack([]); // Clear redo stack when content changes
+    setContent(newContent);
+  };
+
+  const handleBold = () => {
+    applyFormatting('**Bold Text**');
+  };
+
+  const handleItalic = () => {
+    applyFormatting('*Italic Text*');
+  };
+
+  const handleUnderline = () => {
+    applyFormatting('__Underlined Text__');
+  };
+
+  
+
+    
+
+  
+
+
+  const handleUndo = () => {
+    if (undoStack.length > 0) {
+      const newContent = undoStack.pop();
+      setRedoStack([...redoStack, content]);
+      setContent(newContent);
+    }
+  };
+
+  const handleRedo = () => {
+    if (redoStack.length > 0) {
+      const newContent = redoStack.pop();
+      setUndoStack([...undoStack, content]);
+      setContent(newContent);
+    }
   };
 
   return (
     <div>
       <h1>Word Processor</h1>
       <textarea
+        ref={textAreaRef}
         value={content}
         onChange={handleContentChange}
         placeholder="Start typing..."
@@ -19,16 +74,14 @@ function WordProcessor() {
       />
       {/* You can add more formatting and editing features here */}
       <div>
-        <button>Bold</button>
-        <button>Italic</button>
-        <button>Underline</button>
-        {/* Add more formatting options as needed */}
+        <button onClick={handleBold}>Bold</button>
+        <button onClick={handleItalic}>Italic</button>
+        <button onClick={handleUnderline}>Underline</button>
       </div>
       <div>
-        <button>Undo</button>
-        <button>Redo</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleRedo}>Redo</button>
       </div>
-      {/* Add more controls and functionality */}
     </div>
   );
 }
